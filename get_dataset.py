bdb16ac4fbf818d112e5435e8913c15c96926ff5
Charlie Gottlieb <charliegottlieb@gmail.com>
2024-04-14 21:23:00 -0400

    Got 114 london pics

--- get_dataset.py
+++ get_dataset.py
@@ -14,21 +14,25 @@ def get_id_from_pano(pano): #weirdly can't subscript the Panorama object to get 
 
 london = [51.509865, -0.118092] #lat, long. found this online
 
-for lat_add in range(-5, 6): #-5 km to 5 km
-    for long_add in range(-5, 6):
-        lat = london[0] + 0.01 * lat_add #0.01 latitude/longitude is about 1 km
-        lon = london[1] + 0.01 * long_add
+with open("London/__files.csv", "w") as f:
+    f.write("id,lat,lon")
+    for lat_add in range(-5, 6): #-5 km to 5 km
+        for long_add in range(-5, 6):
+            lat = london[0] + 0.01 * lat_add #0.01 latitude/longitude is about 1 km
+            lon = london[1] + 0.01 * long_add
 
-        panos = search_panoramas(lat=lat, lon=lon)
-        if len(panos) > 0:
-            closest = panos[0]
-            id = get_id_from_pano(closest)
+            panos = search_panoramas(lat=lat, lon=lon)
+            if len(panos) > 0:
+                closest = panos[0]
+                id = get_id_from_pano(closest)
 
-            image = get_streetview(
-                pano_id="_R1mwpMkiqa2p0zp48EBJg",
-                api_key=charlie_api_key
-            )
+                image = get_streetview(
+                    pano_id=id,
+                    api_key=charlie_api_key
+                )
 
-            image.save(f"London/{id}.jpg", "jpeg")
+                image.save(f"London/{id}.jpg", "jpeg")
 
-            print(f"Lat: {lat}, long:{lon}, id:{id}")
+                f.write(f"{id},{lat},{lon}")
+                print(f"Lat: {lat}, long:{lon}, id:{id}")
+
